87bddee81bd0b06ab4bdcaed1a6f92c69ec77d2e
Алина Зырянова <Zakirova-alina-m@yandex.ru>
2022-11-24 00:29:08 +0500

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+"""Документация модуля. Описывает работу классов и функций. 
+Размещается в верхней части файла (начиная с первой строки).
+"""
+
 from random import randint
+"""Импорт"""
 
 
-def attack(char_name, char_class):
+from graphic_arts.start_game_banner import run_screensaver
+"""Импортирует функцию"""
+
+
+def attack(char_name: str, char_class: str) -> str:
+    """Описывает работу функции attack."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,7 +24,8 @@ def attack(char_name, char_class):
     return (f'{char_name} не нанес удар')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
+    """Описывает работу функции defence."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -24,7 +35,8 @@ def defence(char_name, char_class):
     return (f'{char_name} не блокировал')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
+    """Описывает работу функции special."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -35,7 +47,8 @@ def special(char_name, char_class):
     return (f'{char_name} не применил умение')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
+    """Описывает работу функции start_training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -59,7 +72,8 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
+def choice_char_class() -> str:
+    """Описывает работу функции choice_char_class."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -81,15 +95,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
